41bd49778080fdf39d4d6620f72bf6cc5bfbacfd
Михаил <113026721+beeshnik@users.noreply.github.com>
2023-08-11 19:57:59 +0500

    add functions

--- main.py
+++ main.py
@@ -2,75 +2,80 @@ import telebot
 from telebot import types
 
 import requests
-
+import json_parsing
 
 bot = telebot.TeleBot('6348506696:AAGHBhAGBYF0I0iHFuzBuPYYdgEYHumg3bQ')
 
-# -----------  Тексты для сообщений бота  ------------- #
-
-# Выбор курса:
-first = '1'
-second = '2'
-third = '3'
-four = '4'
-five = '5'
-
-# Выбор направления:
-superheroes = 'РИС'
-program_engineers = 'ПИ'
-business_informatics = 'БИ'
-international_bachelor = 'МБ'
-history = 'И'
-jurists = 'Ю'
-economics = 'Э'
-languages = 'ИЯ'
-business_management = 'УБ'
-
-# Номер группы:
-
-
 # -----------  Данные пользователя  ------------- #
 
-user_data_list = []   # Данные для идентификации пользователя, 1 - курс, 2 - направление, 3 - группа
+user_data_list = [0] * 3  # Данные для идентификации пользователя, 1 - курс, 2 - направление, 3 - группа
+
+# -----------  Тексты для сообщений бота  ------------- #
+
+text_hello = "Давай познакомися! На каком курсе ты учишься?"
+
 
 # -----------  --------------------------  ------------- #
+# print(json_parsing.courses)
+# print(json_parsing.groups)
+# print(json_parsing.subgroups)
+# print(json_parsing.programs)
+
+# Создание кнопок выбора курса
+def get_course(message):
+    markup = types.InlineKeyboardMarkup()
+    for i in range(len(json_parsing.courses)):
+        markup.add(types.InlineKeyboardButton(json_parsing.courses[i], callback_data=json_parsing.courses[i]))
+
+    bot.send_message(message.chat.id, text_hello, reply_markup=markup)
+
+    @bot.callback_query_handler(get_course(message))
+    def callback_message(callback):
+        bot.delete_message(callback.message.chat.id, callback.message.message_id)
+        user_data_list[0] = callback.data
+        get_program(message)
+
+
+def get_program(message):
+    text_get_course = "Ты выбрал " + str(user_data_list[0]) + " курс! На каком направлении ты учишься?"
+    markup = types.InlineKeyboardMarkup()
+    for i in range(len(json_parsing.programs)):
+        markup.add(types.InlineKeyboardButton(json_parsing.programs[i], callback_data=json_parsing.programs[i]))
+
+    bot.send_message(message.chat.id, text_get_course, reply_markup=markup)
+
+    @bot.callback_query_handler(func=lambda callback: True)
+    def callback_message(callback):
+        bot.delete_message(callback.message.chat.id, callback.message.message_id)
+        user_data_list[1] = callback.data
+        get_group(message)
+
+
+def get_group(message):
+    text_get_group = "Отлично, ты выбрал " + str(user_data_list[1]) + " направление! Теперь давай выберем группу!"
+    markup = types.InlineKeyboardMarkup()
+    for i in range(len(json_parsing.groups)):
+        markup.add(types.InlineKeyboardButton(json_parsing.groups[i], callback_data=json_parsing.groups[i]))
+
+    bot.send_message(message.chat.id, text_get_group, reply_markup=markup)
+
+    @bot.callback_query_handler(func=lambda callback: True)
+    def callback_message(callback):
+        bot.delete_message(callback.message.chat.id, callback.message.message_id)
+        user_data_list[2] = callback.data
+        print(user_data_list)
+
 
 @bot.message_handler(commands=['start'])
 def get_menu(message):
-    # bot.delete_message(message.chat.id, message.message_id)
-    button_course_1 = types.InlineKeyboardButton(first, callback_data=first)
-    button_course_2 = types.InlineKeyboardButton(second, callback_data=second)
-    button_course_3 = types.InlineKeyboardButton(third, callback_data=third)
-    button_course_4 = types.InlineKeyboardButton(four, callback_data=four)
-    button_course_5 = types.InlineKeyboardButton(five, callback_data=five)
+    user_data_list = [0] * 3
+    get_course(message)
 
-    markup = types.InlineKeyboardMarkup()
-    markup.add(button_course_1)
-    markup.add(button_course_2)
-    markup.add(button_course_3)
-    markup.add(button_course_4)
-    markup.add(button_course_5)
 
-    bot.send_message(message.chat.id, 'Давай познакомися! На каком курсе ты учишься?', reply_markup=markup)
-
-@bot.callback_query_handler(func=lambda callback: True)
-def callback_message(callback):
-        if callback.data == first:
-            bot.delete_message(callback.message.chat.id, callback.message.message_id)
-
-        elif callback.data == second:
-            bot.delete_message(callback.message.chat.id, callback.message.message_id)
-
-        elif callback.data == third:
-            bot.delete_message(callback.message.chat.id, callback.message.message_id)
-
-        elif callback.data == four:
-            bot.delete_message(callback.message.chat.id, callback.message.message_id)
-
-        elif callback.data == five:
-            bot.delete_message(callback.message.chat.id, callback.message.message_id)
-
+# @bot.callback_query_handler(func=lambda callback: True)
+# def callback_message(callback):
+#     bot.delete_message(callback.message.chat.id, callback.message.message_id)
+#     user_data_list[0] = callback.data
 
 
 bot.polling(none_stop=True)
-
