efc0595402c066915eefdac37849452f7022453d
Denis Malinin <melowetty@mail.ru>
2023-08-13 18:05:37 +0500

    Added non filter zone for downloading files

--- src/main/kotlin/com/melowetty/hsepermhelper/filters/SecurityFilter.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/filters/SecurityFilter.kt
@@ -69,6 +69,7 @@ class SecurityFilter(
 
     override fun shouldNotFilter(request: HttpServletRequest): Boolean {
         val basePath = env.getProperty("server.servlet.context-path")
+        if (request.requestURI.startsWith("${basePath}${env.getProperty("file-storage.path")}")) return true
         if(request.requestURI.startsWith("${basePath}${env.getProperty("springdoc.swagger-ui.path")}")) return true
         if(request.requestURI.startsWith("${basePath}/swagger-ui")) return true
         if(request.requestURI.startsWith("${basePath}/v3/api-docs")) return true
