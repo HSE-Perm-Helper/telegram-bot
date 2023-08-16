2f3b179a1d4884058775e03eb12bfceec7cd2b37
Denis Malinin <melowetty@mail.ru>
2023-08-16 19:23:45 +0500

    add: endpoint in user controller
    * add: endpoint for get user files

--- src/main/kotlin/com/melowetty/hsepermhelper/controllers/UserController.kt
+++ src/main/kotlin/com/melowetty/hsepermhelper/controllers/UserController.kt
@@ -2,20 +2,28 @@ package com.melowetty.hsepermhelper.controllers
 
 import com.melowetty.hsepermhelper.dto.UserDto
 import com.melowetty.hsepermhelper.models.Response
+import com.melowetty.hsepermhelper.service.UserFilesService
 import com.melowetty.hsepermhelper.service.UserService
+import com.melowetty.hsepermhelper.utils.FileUtils
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.servlet.http.HttpServletRequest
+import org.springframework.core.io.Resource
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.*
+import kotlin.io.path.Path
+import kotlin.io.path.name
 
 @Tag(name = "Пользователи", description = "Взаимодействие с пользователями")
 @RestController
 @RequestMapping("users")
 class UserController(
-    private val userService: UserService
+    private val userService: UserService,
+    private val userFilesService: UserFilesService,
 ) {
     @SecurityRequirement(name = "X-Secret-Key")
     @Operation(
@@ -81,5 +89,13 @@ class UserController(
     ): Response<UserDto> {
         val user = userService.create(dto = userDto)
         return Response(user)
+    }
+
+    @GetMapping("/files/**")
+    fun getUserFile(request: HttpServletRequest): ResponseEntity<Resource> {
+        val path = FileUtils.extractFilePath(request)
+        val filePath = Path(path)
+        val resource = userFilesService.getFile(filePath)
+        return FileUtils.getFileDownloadResponse(resource, filePath.fileName.name)
     }
 }
