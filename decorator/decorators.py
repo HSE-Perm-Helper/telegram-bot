d516053ad8b2e9f76244f2520efcedc6c515ce96
Denis Malinin <melowetty@mail.ru>
2024-09-13 01:37:23 +0500

    fix decorators

--- decorator/decorators.py
+++ decorator/decorators.py
@@ -11,7 +11,7 @@ from message.common_messages import EXCEPTION_MESSAGE
 
 def typing_action(func):
     @wraps(func)
-    def decorator(*args, **kwargs):
+    async def decorator(*args, **kwargs):
         if len(args) > 0 and isinstance(args[0], aiogram.types.Message):
             chat_id = args[0].chat.id
         elif len(args) > 0 and isinstance(args[0], aiogram.types.CallbackQuery):
@@ -20,43 +20,43 @@ def typing_action(func):
             chat_id = None
 
         if chat_id:
-            bot.send_chat_action(chat_id, 'typing')
-        return func(*args, **kwargs)
+            await bot.send_chat_action(chat_id, 'typing')
+        return await func(*args, **kwargs)
 
     return decorator
 
 
 def exception_handler(func):
-    def wrapper(*args, **kwargs):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
         try:
-            return func(*args, **kwargs)
+            return await func(*args, **kwargs)
         except Exception as e:
             traceback.print_exc()
             for arg in args:
                 if isinstance(arg, aiogram.types.Message):
-                    bot.send_message(arg.chat.id, EXCEPTION_MESSAGE)
+                    await bot.send_message(arg.chat.id, EXCEPTION_MESSAGE)
                     break
                 elif isinstance(arg, aiogram.types.CallbackQuery):
-                    bot.send_message(arg.message.chat.id, EXCEPTION_MESSAGE)
+                    await bot.send_message(arg.message.chat.id, EXCEPTION_MESSAGE)
                     break
 
     return wrapper
 
 
 def required_admin(func):
-    def wrapper(*args, **kwargs):
+    @wraps(func)
+    async def wrapper(*args, **kwargs):
         for arg in args:
             if isinstance(arg, aiogram.types.Message):
                 if not is_admin(arg.chat.id):
                     return
                 else:
-                    func(*args, **kwargs)
-                return
+                    return await func(*args, **kwargs)
             elif isinstance(arg, aiogram.types.CallbackQuery):
                 if not is_admin(arg.message.chat.id):
                     return
                 else:
-                    func(*args, **kwargs)
-                return
+                    return await func(*args, **kwargs)
 
     return wrapper
