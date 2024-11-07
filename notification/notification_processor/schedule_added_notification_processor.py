9bf4f7e0c34640cdc91f848420e112dd51286271
Denis Malinin <melowetty@mail.ru>
2024-11-07 13:11:17 +0500

    change system of deleting notifications

--- notification/notification_processor/schedule_added_notification_processor.py
+++ notification/notification_processor/schedule_added_notification_processor.py
@@ -6,8 +6,10 @@ from notification.notification_type import NotificationType
 
 
 class ScheduleAddedNotificationProcessor(BaseNotificationProcessor):
-    async def process(self, notifications: list[BaseNotification]) -> None:
+    async def process(self, notifications: list[BaseNotification]) -> list[BaseNotification]:
         new_schedule: dict[int, list] = {}
+        processed_notification = []
+
         for notification in notifications:
             payload = notification.payload
             users = payload["users"]
@@ -17,6 +19,8 @@ class ScheduleAddedNotificationProcessor(BaseNotificationProcessor):
                     new_schedule[user] = []
                 new_schedule[user].append(payload["targetSchedule"])
 
+            processed_notification.append(notification)
+
         for telegram_id, schedules in new_schedule.items():
             difference = notification_utils.get_difference_schedule(schedules)
             markup = notification_utils.get_markup_schedule(schedules)
@@ -25,8 +29,9 @@ class ScheduleAddedNotificationProcessor(BaseNotificationProcessor):
                 await bot.send_message(telegram_id, f"{notification_utils.NOTIFICATION_EMOJI} Добавлено {difference}!",
                                        reply_markup=markup.as_markup())
             except Exception as e:
-                print(e)
                 pass
+
+        return processed_notification
 
     async def get_notification_type(self) -> NotificationType:
         return NotificationType.SCHEDULE_ADDED
--- notification/notification_processor/service_warning_notification_processor.py
+++ notification/notification_processor/service_warning_notification_processor.py
@@ -5,16 +5,20 @@ from util.logs_utils import send_logs_to_service_admins
 
 
 class ServiceWarningNotificationProcessor(BaseNotificationProcessor):
-    async def process(self, notifications: list[BaseNotification]) -> None:
+    async def process(self, notifications: list[BaseNotification]) -> list[BaseNotification]:
+        processed_notifications = []
         for notification in notifications:
             try:
                 payload = notification.payload
                 message = payload["message"]
 
                 await send_logs_to_service_admins(message)
+                processed_notifications.append(notification)
             except Exception as e:
                 pass
 
+
+        return processed_notifications
 
     async def get_notification_type(self) -> NotificationType:
         return NotificationType.SERVICE_WARNING
